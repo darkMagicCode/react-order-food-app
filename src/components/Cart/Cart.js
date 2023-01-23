660d5040dd54c2eb342d77538271a1f308f11828
darkMagicCode <darkmagiccode@gmail.com>
2023-01-23 14:06:28 +0200

    final code with firebase and checkout adress user

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,52 +1,31 @@
-import React, { useContext } from "react";
-import Cartcontext from "../../store/Cartcontext";
-import Modal from "../UI/Modal";
-import classes from "./Cart.module.css";
-import CartItem from "./CartItem";
+import { useSelector } from 'react-redux';
+
+import Card from '../UI/Card';
+import classes from './Cart.module.css';
+import CartItem from './CartItem';
 
 const Cart = (props) => {
-  const cartCtx = useContext(Cartcontext);
-  const totalAmount = cartCtx.totalAmount.toFixed(2);
-  const hasItems = cartCtx.items.length > 0;
-  const cartItemRemoveHandler = (id) => {
-    cartCtx.removeItem(id)
-
-  };
-  const cartAddRemoveHandler = (item) => {
-    cartCtx.addItem({...item,amount:1})
-  };
-
-  const cartitems = (
-    <ul className={classes["cart-items"]}>
-      {cartCtx.items.map((item) => (
-        <CartItem
-          key={item.id}
-          name={item.name}
-
-          amount={item.amount}
-          price={item.price}
-          onRemove={cartItemRemoveHandler.bind(null ,item.id)}
-          onAdd={cartAddRemoveHandler.bind(null , item)}/>
-         
-      ))}
-    </ul>
-  );
+  const cartItems = useSelector((state) => state.cart.items);
 
   return (
-    <Modal onhideCart={props.onhideCart}>
-      {cartitems}
-      <div className={classes.total}>
-        <span>total amount</span>
-        <span>{totalAmount}</span>
-      </div>
-      <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onhideCart}>
-          close
-        </button>
-
-        {hasItems && <button className={classes.button}>order</button>}
-      </div>
-    </Modal>
+    <Card className={classes.cart}>
+      <h2>Your Shopping Cart</h2>
+      <ul>
+        {cartItems.map((item) => (
+          <CartItem
+            key={item.id}
+            item={{
+              id: item.id,
+              title: item.name,
+              quantity: item.quantity,
+              total: item.totalPrice,
+              price: item.price,
+            }}
+          />
+        ))}
+      </ul>
+    </Card>
   );
 };
+
 export default Cart;
